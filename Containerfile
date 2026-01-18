f781c79b5bee00dc33828e54811df2b071ec5c75
MGS <dane.butler@gmail.com>
2026-01-18 17:31:38 +1000

    Populate files for Aethera

---
+++ Containerfile
@@ -0,0 +1,13 @@
+FROM scratch AS ctx
+COPY build_files /
+
+FROM  ghcr.io/ublue-os/base-main:43
+COPY system_files /
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/build.sh
+   
+RUN bootc container lint
---
+++ build_files/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+dnf5 -y remove kernel kernel-* && rm -r -f /usr/lib/modules/*
+dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
+dnf5 -y install --setopt=install_weak_deps=False kernel-cachyos-lto
+dnf5 -y copr disable bieszczaders/kernel-cachyos-lto
+setsebool -P domain_kernel_load_modules on
+
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y copr enable avengemedia/danklinux
+dnf5 remove -y vim-minimal vim-enhanced firefox firefox-langpacks gnome-keyring \
+nano nano-default-editor 
+
+dnf5 install --setopt=install_weak_deps=False -y fish grim slurp mako alacritty  \
+niri xwayland-satellite xdg-desktop-portal-gtk wl-clipboard wlsunset mate-polkit \
+quickshell matugen cliphist dms dms-greeter material-symbols-fonts neovim greetd \
+greetd-selinux qt6-qtwayland adw-gtk3-theme network-manager-applet x264 x265 rar \
+ffmpeg libvpx libdvdcss imv caja atril mousepad engrampa paperwork
+
+systemctl enable --now greetd
+
+dnf5 clean all
+rm -rf /var/tmp/dnf/cache/
+ostree container commit
+rm -rf /tmp/*
