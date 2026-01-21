06f5ff93c2b019f634f95f9ee9ae1db5de39dedc
MGS <dane.butler@gmail.com>
2026-01-21 20:21:57 +1000

    remove dnf version from filename

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,12 +2,12 @@
 
 set -ouex pipefail
 
-dnf5 -y copr enable avengemedia/dms
-dnf5 -y copr enable avengemedia/danklinux
-dnf5 remove -y vim-minimal vim-enhanced firefox firefox-langpacks gnome-keyring \
+dnf -y copr enable avengemedia/dms
+dnf -y copr enable avengemedia/danklinux
+dnf remove -y vim-minimal vim-enhanced firefox firefox-langpacks gnome-keyring \
 nano nano-default-editor 
-
-dnf5 install --setopt=install_weak_deps=False -y fish grim slurp mako alacritty  \
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
+dnf install --setopt=install_weak_deps=False -y fish grim slurp mako alacritty  \
 niri xwayland-satellite xdg-desktop-portal-gtk wl-clipboard wlsunset mate-polkit \
 quickshell matugen cliphist dms dms-greeter material-symbols-fonts neovim greetd \
 greetd-selinux qt6-qtwayland adw-gtk3-theme network-manager-applet x264 x265 rar \
@@ -15,7 +15,7 @@ ffmpeg libvpx libdvdcss imv caja atril mousepad engrampa paperwork
 
 systemctl enable --now greetd
 
-dnf5 clean all
+dnf clean all
 rm -rf /var/tmp/dnf/cache/
 ostree container commit
 rm -rf /tmp/*
