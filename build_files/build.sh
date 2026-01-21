c3538d79363c7fbb512caa9cfa8be860f3c4f7ea
MGS <dane.butler@gmail.com>
2026-01-21 22:10:28 +1000

    freadd a few

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ nano nano-default-editor
 
 dnf install --setopt=install_weak_deps=False -y  \
 greetd greetd-selinux neovim tcpdump symlinks  \
-dnf5-plugins firewalld NetworkManager-wifi  \
+dnf5-plugins firewalld NetworkManager-wifi open264 zip unzip \
 NetworkManager-bluetooth NetworkManager-openvpn NetworkManager-openvpn-gnome 
 #cups bluez-cups appstream open264 openvpn wget2 zip unzip rsync
 #sane-backends-drivers-cameras sane-backends-drivers-scanners systemd-container
@@ -18,7 +18,7 @@ dnf -y copr enable avengemedia/dms
 dnf -y copr enable avengemedia/danklinux
 dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
 dnf install  --setopt=install_weak_deps=False -y quickshell matugen cliphist \
-dms dms-greeter material-symbols-fontsniri xwayland-satellite xdg-desktop-portal-gtk  \
+dms dms-greeter material-symbols-fonts niri xwayland-satellite xdg-desktop-portal-gtk  \
 fish grim slurp mako alacritty wl-clipboard wlsunset mate-polkit qt6-qtwayland adw-gtk3-theme \
 x264 x265 rar ffmpeg libvpx libdvdcss gstreamer1-plugins-good gstreamer1-plugins-bad-free \
 imv caja atril mousepad engrampa paperwork network-manager-applet
