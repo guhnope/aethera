18654462d8567e1ab431d7e92a82bf1c6c058bb9
MGS <dane.butler@gmail.com>
2026-01-23 17:40:08 +1000

    add drivers

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,22 +5,22 @@ dnf remove -y vim-minimal vim-enhanced firefox firefox-langpacks gnome-keyring \
 nano nano-default-editor 
 
 dnf install -y greetd greetd-selinux  \
-tcpdump symlinks appstream cups zip unzip wget2 bluez-cups dnf5-plugins \
-alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-gstreamer \
+tcpdump symlinks appstream cups zip unzip wget2 bluez-cups dnf5-plugins flatpak \
+alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-gstreamer flatpak-selinux \
 pipewire-alsa pipewire-pulseaudio pipewire-utils pipewire-jack-audio-connection-kit \
-firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManager-openvpn \
-NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd \
-sane-backends-drivers-cameras sane-backends-drivers-scanners rootfiles \
-system-config-printer-udev system-config-printer-libs dhcp-client iwlwifi-mld-firmware
+firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManager-openvpn libva-intel-media-driver \
+NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd fish ImageMagick \
+sane-backends-drivers-cameras sane-backends-drivers-scanners rootfiles dhcp-client \
+system-config-printer-udev system-config-printer-libs intel-vaapi-driver iwlwifi-mld-firmware
 
 dnf -y copr enable avengemedia/dms
 dnf -y copr enable avengemedia/danklinux
 dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
 dnf install  --setopt=install_weak_deps=False -y quickshell matugen cliphist \
 dms dms-greeter material-symbols-fonts niri xwayland-satellite xdg-desktop-portal-gtk  \
-fish grim slurp mako alacritty wl-clipboard wlsunset mate-polkit qt6-qtwayland adw-gtk3-theme \
+grim slurp mako alacritty wl-clipboard wlsunset mate-polkit qt6-qtwayland adw-gtk3-theme \
 x264 x265 rar ffmpeg libdvdcss gstreamer1-plugins-good gstreamer1-plugins-bad-free \
-imv caja atril mousepad engrampa paperwork network-manager-applet ImageMagick flatpak flatpak-selinux
+imv caja atril mousepad engrampa paperwork network-manager-applet   
 
 systemctl enable --now greetd
 systemctl enable --now firewalld
