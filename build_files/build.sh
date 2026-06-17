71882399109bf177e4ed446252864705ebbccc4e
guhnope <dane.butler@gmail.com>
2026-06-17 20:05:51 +1000

    remove fonts

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -ouex pipefail
 
-# 1. Clean out unwanted default packages
 dnf remove -y vim-minimal vim-enhanced firefox firefox-langpacks gnome-keyring nano \
 nano-default-editor
 
-# 2. Base Fedora system packages
 dnf install -y greetd greetd-selinux \
 kernel-modules-extra tcpdump symlinks appstream cups zip unzip wget2 bluez-cups dnf5-plugins \
 alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-gstreamer flatpak flatpak-selinux \
@@ -15,8 +13,7 @@ NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd fish Ima
 sane-backends-drivers-cameras sane-backends-drivers-scanners rootfiles dhcp-client \
 system-config-printer-udev system-config-printer-libs iwlwifi-mld-firmware iwlwifi-mvm-firmware
 
-# Install desktop apps
-dnf install --setopt=install_weak_deps=False -y neovim material-symbols-fonts ffmpeg \
+dnf install --setopt=install_weak_deps=False -y neovim ffmpeg \
 niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako adw-gtk3-theme  \
 gstreamer1-plugins-good gstreamer1-plugins-bad-free imv caja atril mousepad engrampa paperwork
 
@@ -30,20 +27,22 @@ terra-release-multimedia gamescope protontricks heroic-games-launcher-bin steam 
 
 dnf install --setopt=install_weak_deps=False -y rar libdvdcss x264 x265
 
-# 6. Create the system-wide symbolic links for vi and vim targeting neovim
+flatpak override --user --filesystem=$HOME/.themes:ro
+flatpak override --user --filesystem=$HOME/.config/gtk-3.0:ro
+flatpak override --user --filesystem=$HOME/.config/gtk-4.0:ro
+flatpak override --user --env=GTK_THEME=adw-gtk3
+
+
 ln -sf /usr/bin/nvim /usr/bin/vi
 ln -sf /usr/bin/nvim /usr/bin/vim
 
-# 7. Enable system services (No '--now' flag inside container builds)
 systemctl enable greetd
 systemctl enable firewalld
 
-# 8. Safe guard for the Niri documentation rename
 if [ -f /usr/share/doc/niri/wiki/Layer‐Shell-Components.md ]; then
     mv /usr/share/doc/niri/wiki/Layer‐Shell-Components.md /usr/share/doc/niri/wiki/Layer-Shell-Components.md
 fi
 
-# 9. Cleanup caches and commit ostree layer
 dnf clean all
 rm -rf /var/cache/libdnf5/ /var/tmp/dnf/
 ostree container commit
