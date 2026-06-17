a7ff212d55929cbfeb11f8bc6f84f34f72990eea
guhnope <dane.butler@gmail.com>
2026-06-17 21:41:12 +1000

    Add Zed editor

--- build_files/build.sh
+++ build_files/build.sh
@@ -79,6 +79,11 @@ Branch=stable
 Branch=stable
 EOF
 
+[Flatpak Preinstall dev.zed.Zed]
+Branch=stable
+EOF
+
+
 # C. Generate first-boot systemd synchronization manager to run once the network goes online
 cat << 'EOF' > /etc/systemd/system/aethera-flatpak-sync.service
 [Unit]
