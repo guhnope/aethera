3c785c0e75aca880c3cd31693c94ba131557eb66
dbutler <dane.butler@gmail.com>
2026-07-19 19:19:03 +1000

    reintrodice some codecs

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,12 +34,12 @@ dnf install -y terra-release-multimedia terra-release-extras
 
 dnf install --setopt=install_weak_deps=False -y ghostty noctalia noctalia-greeter unrar \
 alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-pulseaudio pipewire-gstreamer \
-pipewire-utils pipewire-jack-audio-connection-kit pipewire-alsa \
-zed helium-browser-bin vesktop
+pipewire-utils pipewire-jack-audio-connection-kit pipewire-alsa x264 x265 \
+zed helium-browser-bin vesktop vulkan-loader mesa-libEGL vulkan-tools gamemode vkBasalt
 #dnf install -y ffmpeg xevd NetworkManager-openvpn-gnome
 #dnf install --setopt=install_weak_deps=False -y fdk-aac ffmpeg steam  heroic-games-launcher \
-#libavcodec libavdevice libavformat libavutil libswresample protonplus terra-gamescope x264 x265
-# vulkan-loader mesa-libEGL vulkan-tools gamemode vkBasalt
+#libavcodec libavdevice libavformat libavutil libswresample protonplus terra-gamescope
+#
 # =====================================================================
 # 5. DECLARATIVE SYSTEM-WIDE FLATPAK PROVISIONING
 # =====================================================================
