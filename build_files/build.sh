aeec49b1f75b0d2b07c56a70f443fd58d21f7722
dbutler <dane.butler@gmail.com>
2026-07-19 11:18:46 +1000

    split out printers, scanners and firmware

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,22 +11,23 @@ nano-default-editor
 # 2. BASE SYSTEM RUNTIMES (CORE REPO LAYER)
 # =====================================================================
 dnf install -y greetd greetd-selinux \
-kernel-modules-extra tcpdump symlinks appstream cups zip unzip wget2 bluez-cups dnf5-plugins \
+kernel-modules-extra tcpdump symlinks appstream  zip unzip wget2 bluez-cups dnf5-plugins \
  flatpak flatpak-selinux dhcp-client firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManager-openvpn libva-intel-media-driver \
-NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd fish \
-sane-backends-drivers-cameras sane-backends-drivers-scanners rootfiles dhcp-client \
-system-config-printer-udev system-config-printer-libs iwlwifi-mld-firmware iwlwifi-mvm-firmware
+NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd fish rootfiles dhcp-client
 
-dnf install --setopt=install_weak_deps=False alsa-firmware alsa-tools-firmware alsa-utils \
-pipewire  pipewire-alsa pipewire-pulseaudio pipewire-utils pipewire-jack-audio-connection-kit
+echo "Firmware"
+dnf install -y  iwlwifi-mld-firmware iwlwifi-mvm-firmware
+echo "Printers and Scanners"
+dnf install -y cups system-config-printer-udev system-config-printer-libs ane-backends-drivers-cameras sane-backends-drivers-scanners
 
-dnf install --setopt=install_weak_deps=False ImageMagick
 # =====================================================================
 # 3. CORE REPO PACKAGES (NO WEAKDEPS)
 # =====================================================================
 dnf install --setopt=install_weak_deps=False -y neovim  \
 niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako adw-gtk3-theme \
-pipewire-gstreamer gstreamer1-plugins-good gstreamer1-plugins-bad-free imv caja atril engrampa paperwork gamescope gamemode
+pipewire-gstreamer gstreamer1-plugins-good gstreamer1-plugins-bad-free imv caja atril engrampa \
+alsa-firmware alsa-tools-firmware alsa-utils pipewire  pipewire-alsa pipewire-pulseaudio \
+pipewire-utils pipewire-jack-audio-connection-kitpaperwork gamescope gamemode ImageMagick
 
 # =====================================================================
 # 4. EXTERNAL REPOSITORY (TERRA)
