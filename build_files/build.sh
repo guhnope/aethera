187182679ce5e1850cb55de1d9a8264e646ff7dc
dbutler <dane.butler@gmail.com>
2026-07-19 19:49:02 +1000

    Minimise from terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,6 +15,8 @@ dhcp-client firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManage
 openvpn systemd-networkd tcpdump iwlwifi-mld-firmware iwlwifi-mvm-firmware libva-intel-media-driver \
 system-config-printer-udev system-config-printer-libs sane-backends-drivers-cameras sane-backends-drivers-scanners \
 cups bluez-cups kernel-modules-extra symlinks zip wget2 dnf5-plugins fish systemd-container rootfiles \
+alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-pulseaudio pipewire-gstreamer \
+pipewire-utils pipewire-jack-audio-connection-kit pipewire-alsa  \
 niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako adw-gtk3-theme \
 paperwork imv caja atril engrampa gamemode gamescope
 
@@ -24,7 +26,8 @@ paperwork imv caja atril engrampa gamemode gamescope
 dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-44.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-44.noarch.rpm
-dnf install -y ffmpeg ffmpegthumbnailer
+dnf install -y ffmpeg ffmpegthumbnailer steam unrar x264 x265 vulkan-tools vkBasalt \
+
 
 # =====================================================================
 # 4. EXTERNAL REPOSITORY (TERRA)
@@ -32,11 +35,8 @@ dnf install -y ffmpeg ffmpegthumbnailer
 wget2 -O /etc/yum.repos.d/terra.repo https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
 dnf install -y terra-release-multimedia terra-release-extras
 
-dnf install --setopt=install_weak_deps=False -y ghostty noctalia noctalia-greeter unrar \
-alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-pulseaudio pipewire-gstreamer \
-pipewire-utils pipewire-jack-audio-connection-kit pipewire-alsa x264 x265 \
-zed helium-browser-bin vesktop vulkan-loader mesa-libEGL vulkan-tools vkBasalt \
-protonplus steam heroic-games-launcher
+dnf install --setopt=install_weak_deps=False -y ghostty noctalia noctalia-greeter  \
+zed helium-browser-bin vesktop protonplus heroic-games-launcher
 #
 # =====================================================================
 # 5. DECLARATIVE SYSTEM-WIDE FLATPAK PROVISIONING
