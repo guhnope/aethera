d135bd401e006784f62849cb03dcc208d0e2b58c
dbutler <dane.butler@gmail.com>
2026-07-19 11:07:27 +1000

    seperate imagemagik and sound

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,14 +12,14 @@ nano-default-editor
 # =====================================================================
 dnf install -y greetd greetd-selinux \
 kernel-modules-extra tcpdump symlinks appstream cups zip unzip wget2 bluez-cups dnf5-plugins \
-alsa-firmware alsa-tools-firmware alsa-utils pipewire flatpak flatpak-selinux \
- dhcp-client \
-firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManager-openvpn libva-intel-media-driver \
-NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd fish ImageMagick \
+ flatpak flatpak-selinux dhcp-client firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManager-openvpn libva-intel-media-driver \
+NetworkManager-openvpn-gnome openvpn systemd-container systemd-networkd fish \
 sane-backends-drivers-cameras sane-backends-drivers-scanners rootfiles dhcp-client \
 system-config-printer-udev system-config-printer-libs iwlwifi-mld-firmware iwlwifi-mvm-firmware
 
-dnf install --setopt=install_weak_deps=False ImageMagick pipewire-alsa pipewire-pulseaudio pipewire-utils pipewire-jack-audio-connection-kit
+dnf install --setopt=install_weak_deps=False alsa-firmware alsa-tools-firmware alsa-utils \
+pipewire ImageMagick pipewire-alsa pipewire-pulseaudio pipewire-utils \
+pipewire-jack-audio-connection-kit
 # =====================================================================
 # 3. CORE REPO PACKAGES (NO WEAKDEPS)
 # =====================================================================
