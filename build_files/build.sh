1d2a6f2ccb705995c0f3de8e0508ff8fb3be3f49
dbutler <dane.butler@gmail.com>
2026-07-19 18:02:41 +1000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,7 +22,7 @@ cups bluez-cups kernel-modules-extra symlinks zip wget2 dnf5-plugins fish system
 wget2 -O /etc/yum.repos.d/terra.repo https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
 dnf install -y terra-release-multimedia terra-release-extras
 
-dnf install --setopt=install_weak_deps=False -y fdk-aac libavcodec libavdevice libavformat libavutil
+dnf install --setopt=install_weak_deps=False -y fdk-aac libavcodec libavdevice libavformat libavutil libswresample
 
 dnf install --setopt=install_weak_deps=False -y ghostty noctalia noctalia-greeter \
 niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako  \
