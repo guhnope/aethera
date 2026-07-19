3d1b632d29e6b5909d293597d81a012bde60e53b
dbutler <dane.butler@gmail.com>
2026-07-19 16:56:06 +1000

    Audio moved to terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,23 +16,19 @@ openvpn systemd-networkd tcpdump iwlwifi-mld-firmware iwlwifi-mvm-firmware libva
 system-config-printer-udev system-config-printer-libs sane-backends-drivers-cameras sane-backends-drivers-scanners \
 cups bluez-cups kernel-modules-extra symlinks zip wget2 dnf5-plugins fish systemd-container rootfiles \
 
-echo "Audio packages"
-dnf -y install --setopt=install_weak_deps=False alsa-firmware alsa-tools-firmware \
-alsa-utils pipewire pipewire-pulseaudio pipewire-utils pipewire-jack-audio-connection-kit \
- pipewire-alsa pipewire-gstreamer
-
-echo "Niri and GUI apps"
-dnf -y install niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako  \
-paperwork imv caja atril engrampa adw-gtk3-theme
-
 # =====================================================================
 # 4. EXTERNAL REPOSITORY (TERRA)
 # =====================================================================
 wget2 -O /etc/yum.repos.d/terra.repo https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
 dnf install -y terra-release-multimedia terra-release-extras
 dnf install --setopt=install_weak_deps=False -y ghostty noctalia noctalia-greeter \
-unrar zed helium-browser-bin x264 x265 vulkan-loader mesa-libEGL vulkan-tools vkBasalt \
-steam terra-gamescope gamemode protonplus heroic-games-launcher vesktop
+niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako  \
+alsa-firmware alsa-tools-firmware alsa-utils pipewire pipewire-pulseaudio fdk-aac \
+pipewire-utils pipewire-jack-audio-connection-kit pipewire-alsa pipewire-gstreamer \
+paperwork imv caja atril engrampa adw-gtk3-theme unrar zed helium-browser-bin x264 x265 \
+vulkan-loader mesa-libEGL vulkan-tools vkBasalt gamemode terra-gamescope \
+steam protonplus heroic-games-launcher vesktop
+
 #dnf install -y ffmpeg xevd NetworkManager-openvpn-gnome gstreamer1-plugins-good gstreamer1-plugins-bad-free
 
 # =====================================================================
