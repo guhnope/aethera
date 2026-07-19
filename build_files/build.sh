a8a64567532b74ea04b3b7f8d0df4895fdae916b
dbutler <dane.butler@gmail.com>
2026-07-19 15:15:50 +1000

    Split Niri and co

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,13 +15,15 @@ dhcp-client firewalld NetworkManager-wifi NetworkManager-bluetooth NetworkManage
 openvpn systemd-networkd tcpdump iwlwifi-mld-firmware iwlwifi-mvm-firmware libva-intel-media-driver \
 system-config-printer-udev system-config-printer-libs sane-backends-drivers-cameras sane-backends-drivers-scanners \
 cups bluez-cups kernel-modules-extra symlinks zip wget2 dnf5-plugins fish systemd-container rootfiles \
-niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako adw-gtk3-theme \
-paperwork imv caja atril engrampa
 
 echo "Audio packages"
 dnf -y install --setopt=install_weak_deps=False alsa-firmware alsa-tools-firmware \
 alsa-utils pipewire pipewire-pulseaudio pipewire-utils pipewire-jack-audio-connection-kit\
 gstreamer1-plugins-good gstreamer1-plugins-bad-free pipewire-alsa pipewire-gstreamer
+
+echo "Niri and GUI apps"
+dnt -y install niri xwayland-satellite matugen cliphist wl-clipboard wlsunset grim slurp mako  \
+paperwork imv caja atril engrampa adw-gtk3-theme
 
 # =====================================================================
 # 4. EXTERNAL REPOSITORY (TERRA)
